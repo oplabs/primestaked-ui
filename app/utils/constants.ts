6998b0066872410254860f0706873fdfda2ddf0d
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-02-04 20:58:00 +0400

    Add rETH and swETH to the config

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -10,6 +10,8 @@ export const assets = [
   { symbol: 'mETH', src: mEthSrc, name: 'Mantle Staked Ether' },
   { symbol: 'ETHx', src: ethxSrc, name: 'Stader ETHx' },
   { symbol: 'sfrxETH', src: sfrxSrc, name: 'Staked Frax Ether' },
+  { symbol: 'swETH', src: sfrxSrc, name: 'Swell ETH' },
+  { symbol: 'rETH', src: sfrxSrc, name: 'Rocket Pool ETH' },
 ] as const
 
 // Ensure there is a contract address for each asset above
@@ -19,6 +21,8 @@ export const contracts = {
   sfrxETH: '0xac3E018457B222d93114458476f3E3416Abbe38F',
   OETH: '0x856c4Efb76C1D1AE02e20CEB03A2A6a08b0b8dC3',
   mETH: '0xd5f7838f5c461feff7fe49ea5ebaf7728bb0adfa',
+  rETH: '0xae78736cd615f374d3085123a210448e74fc6393',
+  swETH: '0xf951e335afb289353dc249e82926178eac7ded78',
 
   primeETH: '0x6ef3D766Dfe02Dc4bF04aAe9122EB9A0Ded25615',
   lrtOracle: '0xA755c18CD2376ee238daA5Ce88AcF17Ea74C1c32',
