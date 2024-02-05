bb426ce0d88e8aae23cd7727486867a2fb8d9704
Micah Alcorn <273937+micahalcorn@users.noreply.github.com>
2024-02-04 22:14:25 -0800

    Link to Uniswap for exits

--- app/routes/app.restake.unstake.tsx
+++ app/routes/app.restake.unstake.tsx
@@ -1,11 +1,10 @@
 import { LargeBox } from '~/components/LargeBox'
+import { ExitContent } from '~/components/ExitContent'
 
 export default function Unstake() {
   return (
     <LargeBox title="Unstake">
-      <div className="bg-white text-center rounded-b-3xl py-24 text-xl font-medium">
-        Unstake is coming soon
-      </div>
+      <ExitContent />
     </LargeBox>
   )
 }
--- app/routes/app.restake.withdraw.tsx
+++ app/routes/app.restake.withdraw.tsx
@@ -1,10 +1,10 @@
 import { LargeBox } from '~/components/LargeBox'
+import { ExitContent } from '~/components/ExitContent'
+
 export default function Withdraw() {
   return (
     <LargeBox title="Withdraw">
-      <div className="bg-white text-center rounded-b-3xl py-24 text-xl font-medium">
-        Withdraw is coming soon
-      </div>
+      <ExitContent />
     </LargeBox>
   )
 }
