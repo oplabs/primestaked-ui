bc738fe8e8a24bc8e1286419b3af96d0555f8c2a
Chris Jacobs <apexearth@gmail.com>
2024-02-06 15:03:48 -0800

    new squid url

--- app/utils/graphql.ts
+++ app/utils/graphql.ts
@@ -1,25 +1,25 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://squid.subsquid.io/origin-squid/v/v69420/graphql',
-});
+  baseURL: 'https://squid.subsquid.io/prime-eth-squid/graphql',
+})
 
 export const graphqlClient =
   <TData, TVariables = unknown>(
     query: string,
     variables?: TVariables,
-    options?: RequestInit["headers"]
+    options?: RequestInit['headers'],
   ) =>
   async () => {
-    const res = await axiosInstance<{data: TData}>({
-      url: "/graphql",
-      method: "POST",
+    const res = await axiosInstance<{ data: TData }>({
+      url: '/graphql',
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       ...options,
       data: { query, variables },
-    });
+    })
 
-    return res.data["data"];
-  };
+    return res.data['data']
+  }
